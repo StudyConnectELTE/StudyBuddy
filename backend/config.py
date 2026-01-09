21e39b49427b5773061d0bb4a52b07a444abf45c
dsNiki <nikidienes195@gmail.com>
2026-01-09 08:47:17 +0100

    database for public url

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@db:3306/studybuddy"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "super-secret-key"
+    # ENV-ből olvassa, local fallback-ként 'db'
+    SQLALCHEMY_DATABASE_URI
